f5d7a3cd6b47e8bd98aa05b57a658b6b424eb513
lalithganeshchalla <challalalithganesh@gmail.com>
2025-09-21 20:10:28 +0530

    add prompts

--- src/components/prompts/data.jsx
+++ src/components/prompts/data.jsx
@@ -112,26 +112,6 @@ const cardsData = [
     tag: "Women",
     text: "A perfect flower chiffon white color pinteresty aesthetic retro saree It must feel like a 90s movie dark brown wavy curly hair with a small flower tucked visibly into her curls.the girl is standing against a solid wall deep shadows and contrast drama lotus flower bookay in her hand while rose petals are showering on her and creating a mysterious and artistic atmosphere where the lighting is worm a golden tones of evoking a sunset or golden hour glow please do not change the face"
   },
-  //   {
-  //   img: Boys1,
-  //   tag: "Men",
-  //   text: "Create a retro, vintage-inspired image grainy yet bright - based on the reference picture. The boy should be dressed in a perfect red, Pinterest-style aesthetic retro kurta. The vibe must capture the essence of a 90s movie brown-haired baddie, with wavy curls, enhanced by a windy, romantic atmosphere. He stands against a solid wall, where deep shadows and dramatic contrasts add mystery and artistry to the scene, creating a moody yet enchanting cinematic effect."
-  // },
-  // {
-  //   img: Girl1,
-  //   tag: "Women",
-  //   text: "Create a retro, vintage-inspired image grainy yet bright - based on the reference picture. The boy should be dressed in a perfect red, Pinterest-style aesthetic retro kurta. The vibe must capture the essence of a 90s movie brown-haired baddie, with wavy curls, enhanced by a windy, romantic atmosphere. He stands against a solid wall, where deep shadows and dramatic contrasts add mystery and artistry to the scene, creating a moody yet enchanting cinematic effect."
-  // },
-  // {
-  //   img: Coples1,
-  //   tag: "Couples",
-  //   text: "Create a retro, vintage-inspired image grainy yet bright based on the reference picture. The girl should be draped in a perfect off-white cotton saree with a red blouse. Pinterest-style aesthetic saree. The vibe must capture the essence of a 90s movie dark brown-haired baddie, enhanced by a windy, romantic atmosphere and the guy should be wearing an off white kurta She stands against an old wooden door, where deep shadows and dramatic contrasts add mystery and artistry to the scene, creating a moody yet enchanting cinematic effect. Make the girl pose like she's walking and looking back while the guy is holding her saree pallu very evidently and the guy should be looking at the girl."
-  // },
-  // {
-  //   img: Boys2,
-  //   tag: "Men",
-  //   text: "Create a retro vintage grainy but bright image of the reference picture but draped in a perfect plain kurta yellow color Pinteresty aesthetic retro kurta. It must feel like a 90s movie dark brown wavy curly hair and romanticising windy environment. The boy is standing against a solid wall deep shadows and contrast drama, creating a mysterious and artistic atmosphere where the lighting is warm with a golden tones of evoking a sunset or golden hour glow. The background is minimalist and slightly textured the expression on his face is moody, calm yet happy and introspective."
-  // },
 
 ];
 
